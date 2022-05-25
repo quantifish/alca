589faf6d051b9f7903a3f6dd70aa41220c11ac9c
quantifish <darcy@quantifish.co.nz>
2022-05-25 13:39:31 +1200

    uploading stuff

--- R/model.R
+++ R/model.R
@@ -9,7 +9,7 @@
 #' @import dplyr
 #' @export
 #' 
-data_to_wide <- function(x, names_from = "length", values_from = "count", ...) {
+data_to_wide <- function(x, names_from = "length", values_from = "count", sum_to_one = FALSE, ...) {
   
   df_wide <- x %>%
     pivot_wider(names_from = names_from, values_from = values_from, names_sort = TRUE, names_prefix = "A.", values_fill = 0, ...)
@@ -18,14 +18,74 @@ data_to_wide <- function(x, names_from = "length", values_from = "count", ...) {
     select(all_of(starts_with("A."))) %>% 
     as.matrix()
   colnames(Y) <- NULL
+  if (sum_to_one) Y <- Y / rowSums(Y)
   
   df <- df_wide %>% 
-    select(!starts_with("A.")) %>% 
+    select(!all_of(starts_with("A."))) %>% 
     data.frame()
   df$size <- rowSums(Y)
   df$Y <- Y
   
   return(df)
+}
+
+
+#' Find the midpoints of a vector of bins
+#' 
+#' @param fit The \code{brms} model fit.
+#' @return a \code{function} containing initial values.
+#' 
+#' @importFrom stringr str_sub str_extract
+#' @export
+#' 
+cut_midpoint <- function(x) {
+  (as.numeric(str_sub(str_extract(x, '(\\(|\\[).*,'), 2, -2)) + as.numeric(str_sub(str_extract(x, ',.*(\\)|\\])'), 2, -2))) / 2
+}
+
+
+#' Aggregate the tails of a matrix
+#' 
+#' @param fit The \code{brms} model fit.
+#' @return a \code{function} containing initial values.
+#' 
+#' @export
+#' 
+aggregate_lf_tails <- function(M, lb = 1, ub) {
+  if (lb > 1) {
+    m1 <- rowSums(M[,1:lb])
+  } else {
+    m1 <- M[,lb]
+  }
+  
+  m2 <- M[,(lb + 1):(ub - 1)]
+  
+  if (ub < ncol(M)) {
+    m3 <- rowSums(M[,ub:ncol(M)])
+  } else {
+    m3 <- M[,ub]
+  }
+  
+  MM <- cbind(m1, m2, m3)
+  colnames(MM) <- NULL
+  return(MM)
+}
+
+
+#' Get initial values.
+#' 
+#' @param fit The \code{brms} model fit.
+#' @return a \code{function} containing initial values.
+#' 
+#' @import dplyr
+#' @export
+#' 
+get_init <- function(fit) {
+  fe <- fixef(fit) %>% 
+    data.frame()
+  init <- as.list(fe$Estimate)
+  names(init) <- rownames(fe)
+  init_fun <- function() init
+  return(init_fun)
 }
 
 
